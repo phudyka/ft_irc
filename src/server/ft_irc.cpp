40f22a1d7a5cb627b5758ac1b881e1fd121d8f09
David TASSEL <davidtassel04@gmail.com>
2024-04-26 11:01:52 +0200

    quit

--- src/server/ft_irc.cpp
+++ src/server/ft_irc.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_irc.cpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: phudyka <phudyka@student.42.fr>            +#+  +:+       +#+        */
+/*   By: dtassel <dtassel@42.nice.fr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 08:59:08 by dtassel           #+#    #+#             */
-/*   Updated: 2024/04/25 11:33:52 by phudyka          ###   ########.fr       */
+/*   Updated: 2024/04/26 11:00:57 by dtassel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -65,7 +65,7 @@ void ft_irc::start()
 	std::cout << YELLOW << "[Welcome to ft_irc]" << RESET << std::endl << std::endl;
 	std::cout << PURPLE << "Waiting for connection..." << RESET << std::endl << std::endl;
     _isRunning = true;
-	//displayClients();
+	displayClients();
     handleConnection();
 }
 
@@ -91,9 +91,9 @@ void ft_irc::handleConnection(void)
             if (_pollfds[i].revents & POLLIN)
             {
                 clientData(i);
-                // displayClients();
+                displayClients();
             }
-            else if (_pollfds[i].revents & POLLHUP)
+            else if (_pollfds[i].revents & (POLLHUP | POLLERR))
             {
                 removeClient(i);
             }
@@ -134,7 +134,7 @@ void ft_irc::newConnection(void)
         std::cerr << RED << "Error: [Fail to get username]" << RESET << std::endl;
         close(UserSocket);
     }
-	// displayClients();
+	displayClients();
 }
 
 void ft_irc::clientData(size_t index)
@@ -168,7 +168,10 @@ void ft_irc::clientData(size_t index)
                 client->_buffer.erase(0, pos + 2);
                 Command commandHandler;
                 if (commandHandler.masterCommand(client, fullCommand, _channels, _pass, _clients) == 1)
-                   close(client->getSocket()); 
+                {
+                    close(client->getSocket()); 
+                    removeClient(index);
+                }
             }
         }
     }
